bec12d506ed3dd2c6a7da8d7d20ed18e073c4248
santimuller45 <santiagomuller45@gmail.com>
2023-11-27 21:19:45 -0300

    27-11-23

--- src/helpers/admin.js
+++ src/helpers/admin.js
@@ -6,6 +6,11 @@ const userAdmin = {
     password: ADM_TORO_PASSWORD,
     firstname: "LIHUEL",
     lastname: "ZELAYA",
+    address: "",
+    cp: 8000,
+    city: "9 DE JULIO",
+    country: "ARGENTINA",
+    phone: 123456789,
     userType: "ADMIN",
     adminType: true
 };

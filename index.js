f27c8dc6e7f251f011b10840c34d3203297c4331
santimuller45 <santiagomuller45@gmail.com>
2024-04-22 20:07:01 -0300

    22-04-24

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const { conn } = require('./src/db.js');
 const { PORT } = process.env;
 
 const { getHelperMenu } = require('./src/controllers/foodControllers.js');
-const { createAdmin } = require("./src/controllers/usersControllers.js");
+const { createAdmin } = require('./src/controllers/usersControllers.js');
+// const { createCategorys } = require('./src/controllers/categoryControllers.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then( async () => {  // cambio a alter en lugar de force
     await getHelperMenu();
     await createAdmin();
+    // await createCategorys();
     app.listen( PORT, () => {
         console.log('Servidor corriendo en el puerto' , PORT);
     });
